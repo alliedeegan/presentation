d8bd268d235ce0fda4d3eda5521c72597d9b5e66
Allie <alicemdeegan@gmail.com>
2019-03-14 12:15:33 -0400

    jquery bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-$(function () {
-    particlesJS.load('particles-header', 'scripts/particlesjsDefault.json');
-    particlesJS.load('particles-colorWizard', 'scripts/particles2.json');
-    particlesJS.load('particles-night', 'scripts/particlesjs-night.json');
-    particlesJS.load('particles-snow', 'scripts/particlesjs-snow.json');
-    particlesJS.load('particles-apples', 'scripts/particlesjs-apples.json');
-    particlesJS.load('particles-portfolio', 'scripts/particlesjs-portfolio.json');
-    particlesJS.load('particles-absolute', 'scripts/particlesjsDefault2.json');
-    particlesJS.load('particles-cat', 'scripts/particlesjs-cat.json');
+particlesJS.load('particles-header', 'scripts/particlesjsDefault.json');
+particlesJS.load('particles-colorWizard', 'scripts/particles2.json');
+particlesJS.load('particles-night', 'scripts/particlesjs-night.json');
+particlesJS.load('particles-snow', 'scripts/particlesjs-snow.json');
+particlesJS.load('particles-apples', 'scripts/particlesjs-apples.json');
+particlesJS.load('particles-portfolio', 'scripts/particlesjs-portfolio.json');
+particlesJS.load('particles-absolute', 'scripts/particlesjsDefault2.json');
+particlesJS.load('particles-cat', 'scripts/particlesjs-cat.json');
 
+$(function () {
 
     $('.next').on('click', function () {
         $.smoothScroll('+=' + $(window).height());
