164a1958b605534f95061767df7a01b099be08eb
Allie <alicemdeegan@gmail.com>
2019-03-14 12:11:04 -0400

    fixing bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
-particlesJS.load('particles-header', 'scripts/particlesjsDefault.json');
-particlesJS.load('particles-colorWizard', 'scripts/particles2.json');
-particlesJS.load('particles-night', 'scripts/particlesjs-night.json');
-particlesJS.load('particles-snow', 'scripts/particlesjs-snow.json');
-particlesJS.load('particles-apples', 'scripts/particlesjs-apples.json');
-particlesJS.load('particles-portfolio', 'scripts/particlesjs-portfolio.json');
-particlesJS.load('particles-absolute', 'scripts/particlesjsDefault2.json');
-particlesJS.load('particles-cat', 'scripts/particlesjs-cat.json');
+$(function () {
+    particlesJS.load('particles-header', 'scripts/particlesjsDefault.json');
+    particlesJS.load('particles-colorWizard', 'scripts/particles2.json');
+    particlesJS.load('particles-night', 'scripts/particlesjs-night.json');
+    particlesJS.load('particles-snow', 'scripts/particlesjs-snow.json');
+    particlesJS.load('particles-apples', 'scripts/particlesjs-apples.json');
+    particlesJS.load('particles-portfolio', 'scripts/particlesjs-portfolio.json');
+    particlesJS.load('particles-absolute', 'scripts/particlesjsDefault2.json');
+    particlesJS.load('particles-cat', 'scripts/particlesjs-cat.json');
 
 
-$('.next').on('click', function () {
-    $.smoothScroll('+=' + $(window).height());
+    $('.next').on('click', function () {
+        $.smoothScroll('+=' + $(window).height());
+    });    
 });
